7d4ec39dc9355702e987288d045ed22253bee24f
abbos91 <abbos.shomurodov91@gmail.com>
2026-05-14 16:34:45 +0500

    html and css files updated

---
+++ script.js
@@ -0,0 +1,7 @@
+const cardNumberInput = document.getElementById("cardNumber");
+const expiryInput = document.getElementById("expiry");
+const cvvInput = document.getElementById("cvv");
+const nameInput = document.getElementById("name");
+const form = document.getElementById("paymentForm");
+const modal = document.getElementById("successModal");
+const closeModalBtn = document.getElementById("closeModal");
