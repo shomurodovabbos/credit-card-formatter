58cbaa7bec367cd044f56b08b460fffd098813c6
abbos91 <abbos.shomurodov91@gmail.com>
2026-05-14 16:58:51 +0500

    adding space after every four digits

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ const nameInput = document.getElementById("name");
 const form = document.getElementById("paymentForm");
 const modal = document.getElementById("successModal");
 const closeModalBtn = document.getElementById("closeModal");
+
+cardNumberInput.addEventListener("input", (e) => {
+    let value = e.target.value.replace(/\D/g, "");
+    value = value.substring(0, 16);
+
+    //add space after every 4 digits
+    let formatted = "";
+    for (let i = 0; i < value.length; i++) {
+        if (i % 4 === 0 && i > 0) {
+            formatted += " ";
+        }
+        formatted += value[i];
+    }
+    e.target.value = formatted;
+});
